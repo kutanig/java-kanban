9e2be5b0246cd9548d349770fb3a5d476ba9426c
anatoliyPopan <galaxy628@yandex.ru>
2025-02-27 05:54:18 +0200

    split_9HW v0.2

--- src/http/HttpTaskServer.java
+++ src/http/HttpTaskServer.java
@@ -1,5 +1,7 @@
 package http;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import com.sun.net.httpserver.HttpServer;
 import http.handler.*;
 import manager.Managers;
@@ -34,13 +36,23 @@ public class HttpTaskServer {
         taskManager.add(new Task("task", "des", Taskstatus.NEW));
         taskManager.add(new Epic("epic", "des", Taskstatus.DONE));
         taskManager.add(new Subtask("sub", "dis", Taskstatus.NEW, LocalDateTime.now(), Duration.ofMinutes(5), 2));
-        taskManager.add(new Task("task2", "dasd", Taskstatus.IN_PROGRESS));
+        taskManager.add(new Task("task2", "dasd", Taskstatus.IN_PROGRESS,LocalDateTime.of(2025,5,5,5,5
+        ),Duration.ofDays(1)));
+
         try {
             server.start();
         } catch (IOException e) {
             server.stop();
             System.out.println("Network error: " + e.getMessage());
         }
+    }
+
+    public static Gson getGson() {
+        return new GsonBuilder()
+                .setPrettyPrinting()
+                .registerTypeAdapter(LocalDateTime.class, new BaseHttpHandler.LocalDateTimeAdapters())
+                .registerTypeAdapter(Duration.class, new BaseHttpHandler.DurationAdapters())
+                .create();
     }
 
     public void start() throws IOException {
---
+++ test/HttpTaskManagerTasksTest.java
@@ -0,0 +1,83 @@
+import com.google.gson.Gson;
+import http.HttpTaskServer;
+import manager.InMemoryTaskManager;
+import manager.TaskManager;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.AfterEach;
+import task.Epic;
+import task.Task;
+import task.Taskstatus;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.util.List;
+
+public class HttpTaskManagerTasksTest {
+
+    // создаём экземпляр InMemoryTaskManager
+    TaskManager manager = new InMemoryTaskManager();
+    // передаём его в качестве аргумента в конструктор HttpTaskServer
+    HttpTaskServer taskServer = new HttpTaskServer(manager);
+    Gson gson = HttpTaskServer.getGson();
+
+    public HttpTaskManagerTasksTest() throws IOException, IOException {
+    }
+
+    @BeforeEach
+    public void setUp() throws IOException {
+        manager.clearTasks();
+        manager.clearSubtasks();
+        manager.clearEpics();
+        taskServer.start();
+
+    }
+
+    @AfterEach
+    public void shutDown() {
+        taskServer.stop();
+    }
+
+    @Test
+    public void testAddTask() throws IOException, InterruptedException {
+        // создаём задачу
+        Task task = new Task("Test 2", "Testing task 2", Taskstatus.NEW, LocalDateTime.now(), Duration.ofMinutes(5));
+        // конвертируем её в JSON
+        String taskJson = gson.toJson(task);
+        System.out.println(taskJson);
+
+        // создаём HTTP-клиент и запрос
+        HttpClient client = HttpClient.newHttpClient();
+        URI url = URI.create("http://localhost:8080/tasks");
+        HttpRequest request = HttpRequest.newBuilder().uri(url).POST(HttpRequest.BodyPublishers.ofString(taskJson)).build();
+
+        // вызываем рест, отвечающий за создание задач
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        // проверяем код ответа
+        Assertions.assertEquals(200, response.statusCode());
+
+        // проверяем, что создалась одна задача с корректным именем
+        List<Task> tasksFromManager = manager.getTasks();
+
+        Assertions.assertNotNull(tasksFromManager);
+        Assertions.assertEquals(1, tasksFromManager.size());
+        Assertions.assertEquals(tasksFromManager.get(0).getName(), "Test 2", "Некорректное имя задачи");
+    }
+
+    @Test
+    public void testGetEpics() throws IOException, InterruptedException {
+        //Epic epic = new Epic("epic","asd",Taskstatus.NEW);
+        //manager.add(epic);
+        HttpClient client = HttpClient.newHttpClient();
+        URI url = URI.create("http://localhost:8080/epics");
+        HttpRequest request = HttpRequest.newBuilder().uri(url).GET().build();
+        HttpResponse<String> response = client.send(request,HttpResponse.BodyHandlers.ofString());
+        Assertions.assertEquals(200,response.statusCode());
+    }
+}
